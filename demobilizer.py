3c9b10c08cb53a4c43300dd1c035f6b93559322a
zd9 <zd9@users.noreply.github.com>
2014-02-09 09:49:36 -0800

    Updated for 2.7.6, added support...
    ...for ignoring subreddits/being banned from subreddits

--- demobilizer.py
+++ demobilizer.py
@@ -5,7 +5,7 @@
 # Python Script for making mobile URLs non-mobile URLs
 #  By /u/zd9
 #
-# Written in Python 2.7.5
+# Written in Python 2.7.6
 #===============
 
 import praw
@@ -30,35 +30,44 @@ already_done = set(pickle.load(pickle_file))
 pickle_file.close()
 
 for comment in subreddit_comments:
-    pattern1 = re.compile("(?P<mobile>://(?:.{1,12}\.)?m\.)")
-    match = pattern1.search(comment.body)
+    #Add subreddits the bot is banned from or ignoring here with the code:'subreddit1' or 'subreddit2' or ...
+    #If the bot is not banned anywhere, leave as is.
+    if comment.subreddit != '':
+        pattern1 = re.compile("(?P<mobile>://(?:.{1,12}\.)?m\.)")
+        match = pattern1.search(comment.body)
+        
+        if not match:
+            continue
     
-    if not match:
-        continue
-
-    if match and comment.id not in already_done:
-
-        url_pattern = re.compile("(?P<url>http.{1,}?(?:\s|\)|\Z))")
-
-        url = url_pattern.search(comment.body)
-
-        if not url:
+        if match and comment.id not in already_done:
+    
+            url_pattern = re.compile("(?P<url>http.{1,}?(?:\s|\)|\Z))")
+    
+            url = url_pattern.search(comment.body)
+    
+            if not url:
+                pickle_mod = open('processed.txt', 'w')
+                already_done.add(comment.id)
+                pickle.dump(already_done, pickle_mod)
+                pickle_mod.close()
+                continue
+            if url:
+                desk = url.group('url')
+                desk_url = desk.replace('m.', '')
+                desk_url = desk_url.rstrip(')')
+            
+                comment.reply(u"I've detected a mobile URL in your comment.\n\n**[Here's](%s) the equivalent non-mobile URL**.\n\n---\n\n*^[Bugs/Questions/Suggestions/Improvements?](http://www.reddit.com/message/compose/?to=zd9&subject=RE:+demobilizer+bot) ^| ^[Source\xa0Code](https://github.com/zd9/demobilizer_bot)*" % (desk_url))
+            
             pickle_mod = open('processed.txt', 'w')
             already_done.add(comment.id)
             pickle.dump(already_done, pickle_mod)
             pickle_mod.close()
-            continue
-        if url:
-            desk = url.group('url')
-            desk_url = desk.replace('m.', '')
-            desk_url = desk_url.rstrip(')')
-        
-            comment.reply(u"I've detected a mobile URL in your comment.\n\n**[Here's](%s) the equivalent non-mobile URL**.\n\n---\n\n*^[Bugs/Questions/Suggestions/Improvements?](http://www.reddit.com/message/compose/?to=zd9&subject=RE:+demobilizer+bot) ^| ^[Source\xa0Code](https://github.com/zd9/demobilizer_bot)*" % (desk_url))
-        
+    else:
         pickle_mod = open('processed.txt', 'w')
         already_done.add(comment.id)
         pickle.dump(already_done, pickle_mod)
         pickle_mod.close()
+        continue
 
 
 #===============
